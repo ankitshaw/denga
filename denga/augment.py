6f6145fa51c2e5d11b92d16ab2ccc8b1048c8b9b
Schezeen Fazulbhoy <schezeenf10@gmail.com>
2020-04-02 18:52:49 +0530

    added noun-check for WOI

--- denga/augment.py
+++ denga/augment.py
@@ -2,7 +2,7 @@ import nltk
 import pandas as pd
 from nltk.corpus import stopwords,wordnet
 from nltk.tokenize import word_tokenize, sent_tokenize
-from nltk.tag import DefaultTagger 
+from nltk.tag import DefaultTagger
 
 stop_words = set(stopwords.words('english'))
 def removeStopWords(sentence):
@@ -28,13 +28,13 @@ def getSynonyms(word):
 def getWordsOfInterest(tags):
 	woi = []
 	for (word, tag) in tags:
-		if tag.startswith("J") or tag.startswith("R") or tag.startswith("V"):
+		if tag.startswith("J") or tag.startswith("R") or tag.startswith("V") or tag.startswith("N"):
 			woi.append(word)
 	return woi
 
 def nlp(filePath):
 	df = pd.read_csv(filePath, sep='\t',header= None, error_bad_lines=False)  #need to handle different file formats.
-	df = pd.DataFrame(data, columns=['Sentences'])
+	# df = pd.DataFrame(data, columns=['Sentences'])
 	datasetList = df.iloc[:,0].values.tolist()
 
 	for sentence in datasetList:
@@ -47,35 +47,24 @@ def nlp(filePath):
 			synonym_dict[word] = set(synonyms)
 		print(synonym_dict)
 
-
-
 def nlp_test():
 	# df = pd.read_csv(filePath,sep='\t',header= None, error_bad_lines=False)  #need to handle different file formats.
-	data = ['I will be logging in from home today', 'I will working from home today']
+	data = ['I will be logging in from home today'] # 'I will work from home today'
 	df= pd.DataFrame(data, columns=['Sentences'])
 	datasetList = df.iloc[:,0].values.tolist() #first column of data frame i.e. all sentences
-
-	# pos tagging to know which words can be replaced with their synonyms/hypernym/hyponym (maybe not required???)
 
 	# get list of synonyms for each word in filtered sentence
 	for sentence in datasetList:
 		sentenceFiltered = removeStopWords(sentence)
+		print("filtered sentence: ", sentenceFiltered)
 		tag = getPosTag(sentenceFiltered)
+		print("tag: ", tag)
 		woi = getWordsOfInterest(tag)
+		print("woi: ", woi)
 		synonym_dict = {}
 		for word in woi:
 			synonyms = getSynonyms(word)
 			synonym_dict[word] = set(synonyms)
 		print(synonym_dict)
 
-
-	# generate all combinations of new sentences
-
-	# check grammatical validity of sentences
-
-	# generatedDataset = [getSynonym(x) for x in datasetList]
-
-	# return generatedDataset
-
 nlp_test()
-
