bc2078b725d5a01e9f2bb91e03240338036d08e7
Schezeen Fazulbhoy <schezeenf10@gmail.com>
2020-04-07 22:49:16 +0530

    sentence-generation

--- denga/augment.py
+++ denga/augment.py
@@ -2,12 +2,13 @@ import nltk
 import pandas as pd
 from nltk.corpus import stopwords,wordnet
 from nltk.tokenize import word_tokenize, sent_tokenize
-from nltk.tag import DefaultTagger 
+from nltk.tag import DefaultTagger
+import spacy
 
 stop_words = set(stopwords.words('english'))
 def removeStopWords(sentence):
-		filtered_sentence = " ".join(filter(lambda word: word not in stop_words, sentence.split()))
-		return filtered_sentence
+	filtered_sentence = " ".join(filter(lambda word: word not in stop_words, sentence.split()))
+	return filtered_sentence
 
 def getPosTag(sentence):
 	text = word_tokenize(sentence)
@@ -28,13 +29,13 @@ def getSynonyms(word):
 def getWordsOfInterest(tags):
 	woi = []
 	for (word, tag) in tags:
-		if tag.startswith("J") or tag.startswith("R") or tag.startswith("V"):
+		if tag.startswith("J") or tag.startswith("R") or tag.startswith("V") or tag.startswith("N"):
 			woi.append(word)
 	return woi
 
 def nlp(filePath):
 	df = pd.read_csv(filePath, sep='\t',header= None, error_bad_lines=False)  #need to handle different file formats.
-	df = pd.DataFrame(data, columns=['Sentences'])
+	# df = pd.DataFrame(data, columns=['Sentences'])
 	datasetList = df.iloc[:,0].values.tolist()
 
 	for sentence in datasetList:
@@ -47,35 +48,39 @@ def nlp(filePath):
 			synonym_dict[word] = set(synonyms)
 		print(synonym_dict)
 
-
-
 def nlp_test():
 	# df = pd.read_csv(filePath,sep='\t',header= None, error_bad_lines=False)  #need to handle different file formats.
-	data = ['I will be logging in from home today', 'I will working from home today']
+	data = ["i want to swim"] # 'I will work from home today'
 	df= pd.DataFrame(data, columns=['Sentences'])
 	datasetList = df.iloc[:,0].values.tolist() #first column of data frame i.e. all sentences
 
-	# pos tagging to know which words can be replaced with their synonyms/hypernym/hyponym (maybe not required???)
-
 	# get list of synonyms for each word in filtered sentence
 	for sentence in datasetList:
+		print("Original Sentence: ", sentence)
 		sentenceFiltered = removeStopWords(sentence)
+		print("filtered sentence: ", sentenceFiltered)
 		tag = getPosTag(sentenceFiltered)
+		print("tag: ", tag)
 		woi = getWordsOfInterest(tag)
+		print("woi: ", woi)
 		synonym_dict = {}
 		for word in woi:
 			synonyms = getSynonyms(word)
-			synonym_dict[word] = set(synonyms)
+			synonym_dict[word] = list(set(synonyms))
 		print(synonym_dict)
 
-
-	# generate all combinations of new sentences
-
-	# check grammatical validity of sentences
-
-	# generatedDataset = [getSynonym(x) for x in datasetList]
-
-	# return generatedDataset
+		print("no of woi: ", len(woi))
+		no_of_words_to_be_replaced = len(woi) #flag to set how many words need to be replaced
+		generated_sentences=[]
+		if no_of_words_to_be_replaced <=len(woi):
+			for i in range(0,no_of_words_to_be_replaced):
+				# print(list(synonym_dict.values())[i]) #some check needs to be there to map lemma with actual word in sentence: Lets --> Let
+				for syn in list(synonym_dict.values())[i]:
+					generated_sentence = sentence.replace(list(synonym_dict.keys())[i], syn)
+					# here keep a check based on similarity threshold, only if it passes: append to final list
+					generated_sentences.append(generated_sentence)
+		print(sep="\n",*generated_sentences)
 
 nlp_test()
 
+# https://nlpforhackers.io/wordnet-sentence-similarity/
