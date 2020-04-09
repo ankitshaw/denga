1db49cfa8a7b9506f4bfaa16166acb7499d448d4
Schezeen Fazulbhoy <schezeenf10@gmail.com>
2020-04-09 23:49:18 +0530

    calculates similarity, sentence filtering based on score (#12)
    * calculates similarity, sentence filtering based on score
    
    * passing filtered original sentence to calculateSimilarity()
    
    * lemmatizing generated sentences before similarity check
    
    * handled ZeroDivisionError while calculating average score
    
    * flushing out all_sentence_score dictionary after each iteration

--- denga/augment.py
+++ denga/augment.py
@@ -2,10 +2,15 @@ import nltk
 import pandas as pd
 from nltk.corpus import stopwords,wordnet
 from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.stem import WordNetLemmatizer
 from nltk.tag import DefaultTagger
+import operator
 import spacy
 
-stop_words = set(stopwords.words('english'))
+
+stop_words = set(stopwords.words('english')) #from nltk
+lang_data = spacy.load('en_core_web_lg') #nlp english data model
+
 def removeStopWords(sentence):
 	filtered_sentence = " ".join(filter(lambda word: word not in stop_words, sentence.split()))
 	return filtered_sentence
@@ -14,6 +19,13 @@ def getPosTag(sentence):
 	text = word_tokenize(sentence)
 	tags = nltk.pos_tag(text)
 	return tags
+
+def getWordsOfInterest(tags):
+	woi = []
+	for (word, tag) in tags:
+		if tag.startswith("J") or tag.startswith("R") or tag.startswith("V") or tag.startswith("N"):
+			woi.append(word)
+	return woi
 
 def getSynonyms(word):
 	"""This function returns a list of synonyms for each word passed to it"""
@@ -26,12 +38,38 @@ def getSynonyms(word):
 			synonyms.append(l.name())
 	return synonyms
 
-def getWordsOfInterest(tags):
-	woi = []
-	for (word, tag) in tags:
-		if tag.startswith("J") or tag.startswith("R") or tag.startswith("V") or tag.startswith("N"):
-			woi.append(word)
-	return woi
+#lemmatisation starts here
+def nltk_to_wordnet_tag(nltk_tag):
+    if nltk_tag.startswith('J'):
+        return wordnet.ADJ
+    elif nltk_tag.startswith('V'):
+        return wordnet.VERB
+    elif nltk_tag.startswith('N'):
+        return wordnet.NOUN
+    elif nltk_tag.startswith('R'):
+        return wordnet.ADV
+    else:
+        return None
+
+lemmatizer = WordNetLemmatizer()
+def lemmatize(sentence):
+    nltk_tagged = getPosTag(sentence)
+    wordnet_tagged = map(lambda x: (x[0], nltk_to_wordnet_tag(x[1])), nltk_tagged)
+    lemmatized_sentence = []
+    for word, tag in wordnet_tagged:
+        if tag is None:
+            lemmatized_sentence.append(word)
+        else:
+            lemmatized_sentence.append(lemmatizer.lemmatize(word, tag))
+    return " ".join(lemmatized_sentence)
+
+def calculateSimilarity(sentence1,sentence2):
+	lemmatized_sentence2 = lemmatizer.lemmatize(sentence2) #pre-processing generated sentence
+	sentence2_filtered = removeStopWords(lemmatized_sentence2)
+	# similarity_score = lang_data(sentence1).similarity(lang_data(sentence2))
+	similarity_score_f = lang_data(sentence1).similarity(lang_data(sentence2_filtered))
+	return similarity_score_f #,similarity_score
+
 
 def nlp(filePath):
 	df = pd.read_csv(filePath, sep='\t',header= None, error_bad_lines=False)  #need to handle different file formats.
@@ -50,17 +88,20 @@ def nlp(filePath):
 
 def nlp_test():
 	# df = pd.read_csv(filePath,sep='\t',header= None, error_bad_lines=False)  #need to handle different file formats.
-	data = ["i want to swim"] # 'I will work from home today'
+	data = ["number","two"]
 	df= pd.DataFrame(data, columns=['Sentences'])
 	datasetList = df.iloc[:,0].values.tolist() #first column of data frame i.e. all sentences
 
 	# get list of synonyms for each word in filtered sentence
+	no_of_generated_sentences=0
+	total_score = 0
+	all_sentence_score = {}
 	for sentence in datasetList:
 		print("Original Sentence: ", sentence)
 		sentenceFiltered = removeStopWords(sentence)
 		print("filtered sentence: ", sentenceFiltered)
 		tag = getPosTag(sentenceFiltered)
-		print("tag: ", tag)
+		print("tags: ", tag)
 		woi = getWordsOfInterest(tag)
 		print("woi: ", woi)
 		synonym_dict = {}
@@ -74,12 +115,23 @@ def nlp_test():
 		generated_sentences=[]
 		if no_of_words_to_be_replaced <=len(woi):
 			for i in range(0,no_of_words_to_be_replaced):
-				# print(list(synonym_dict.values())[i]) #some check needs to be there to map lemma with actual word in sentence: Lets --> Let
 				for syn in list(synonym_dict.values())[i]:
 					generated_sentence = sentence.replace(list(synonym_dict.keys())[i], syn)
-					# here keep a check based on similarity threshold, only if it passes: append to final list
-					generated_sentences.append(generated_sentence)
-		print(sep="\n",*generated_sentences)
+					no_of_generated_sentences = no_of_generated_sentences + 1
+					score = calculateSimilarity(sentenceFiltered,generated_sentence)
+					all_sentence_score[generated_sentence]=score # dictionary to map sentence to score
+					total_score = total_score + score
+		try:
+			average = total_score/no_of_generated_sentences
+		except Exception as e:
+			average = 0
+		for sentence,score in all_sentence_score.items():
+			if score >=average: #filtering out only sentences with score above average
+				generated_sentences.append(sentence)
+		all_sentence_score.clear() #flushing out dictionary after sentences generated for each sentence in dataframe
+		print(generated_sentences)
+		# print(dict(sorted(all_sentence_score.items(), key=operator.itemgetter(1), reverse=True)[:5]))
+		# return generated_sentences
 
 nlp_test()
 
--- denga/try.py
+++ denga/try.py
@@ -1,169 +1,223 @@
-import nltk
-from nltk.corpus import stopwords,wordnet
-from nltk.tokenize import word_tokenize, sent_tokenize
-from nltk.tag import DefaultTagger
-import spacy
-
-stop_words = set(stopwords.words('english'))
-def removeStopWords(sentence):
-    filtered_sentence = " ".join(filter(lambda word: word not in stop_words, sentence.split()))
-    return filtered_sentence
-
-nlp = spacy.load('en_core_web_lg')
-a = "i want to swim"
-a1=nlp(removeStopWords(a))
-print(a1)
-b = "i neediness to swim"
-b1=nlp(removeStopWords(b))
-print(b1)
-print(nlp(a).similarity(nlp(b)))
-
-
-
-# # # Program to measure similarity between
-# # # two sentences using cosine similarity.
-# # from nltk.corpus import stopwords
-# # from nltk.tokenize import word_tokenize
-# #
-# # # X = input("Enter first string: ").lower()
-# # # Y = input("Enter second string: ").lower()
-# # X = "I want to swim"
-# # Y = "I wishing to swim"
-# #
-# # # tokenization
-# # X_list = word_tokenize(X)
-# # Y_list = word_tokenize(Y)
-# #
-# # # sw contains the list of stopwords
-# # sw = stopwords.words('english')
-# # l1 = [];
-# # l2 = []
-# #
-# # # remove stop words from string
-# # X_set = {w for w in X_list if not w in sw}
-# # Y_set = {w for w in Y_list if not w in sw}
-# #
-# # # form a set containing keywords of both strings
-# # rvector = X_set.union(Y_set)
-# # for w in rvector:
-# #     if w in X_set:
-# #         l1.append(1)  # create a vector
-# #     else:
-# #         l1.append(0)
-# #     if w in Y_set:
-# #         l2.append(1)
-# #     else:
-# #         l2.append(0)
-# # c = 0
-# #
-# # # cosine formula
-# # for i in range(len(rvector)):
-# #     c += l1[i] * l2[i]
-# # cosine = c / float((sum(l1) * sum(l2)) ** 0.5)
-# # print("similarity: ", cosine)
+# import nltk
+# from nltk.stem import WordNetLemmatizer
+# from nltk.corpus import wordnet
 #
-# from nltk import word_tokenize, pos_tag
-# from nltk.corpus import wordnet as wn
+# lemmatizer = WordNetLemmatizer()
 #
-#
-# def penn_to_wn(tag):
-#     """ Convert between a Penn Treebank tag to a simplified Wordnet tag """
-#     if tag.startswith('N'):
-#         return 'n'
-#
-#     if tag.startswith('V'):
-#         return 'v'
-#
-#     if tag.startswith('J'):
-#         return 'a'
-#
-#     if tag.startswith('R'):
-#         return 'r'
-#
-#     return None
-#
-#
-# def tagged_to_synset(word, tag):
-#     wn_tag = penn_to_wn(tag)
-#     if wn_tag is None:
+# # function to convert nltk tag to wordnet tag
+# def nltk_tag_to_wordnet_tag(nltk_tag):
+#     if nltk_tag.startswith('J'):
+#         return wordnet.ADJ
+#     elif nltk_tag.startswith('V'):
+#         return wordnet.VERB
+#     elif nltk_tag.startswith('N'):
+#         return wordnet.NOUN
+#     elif nltk_tag.startswith('R'):
+#         return wordnet.ADV
+#     else:
 #         return None
 #
-#     try:
-#         return wn.synsets(word, wn_tag)[0]
-#     except:
-#         return None
+# def lemmatize_sentence(sentence):
+#     #tokenize the sentence and find the POS tag for each token
+#     nltk_tagged = nltk.pos_tag(nltk.word_tokenize(sentence))
+#     print(nltk_tagged)
+#     #tuple of (token, wordnet_tag)
+#     wordnet_tagged = map(lambda x: (x[0], nltk_tag_to_wordnet_tag(x[1])), nltk_tagged)
+#     lemmatized_sentence = []
+#     for word, tag in wordnet_tagged:
+#         if tag is None:
+#             #if there is no available tag, append the token as is
+#             lemmatized_sentence.append(word)
+#         else:
+#             #else use the tag to lemmatize the token
+#             lemmatized_sentence.append(lemmatizer.lemmatize(word, tag))
+#     return " ".join(lemmatized_sentence)
+#
+# print(lemmatizer.lemmatize("I am loving it")) #I am loving it
+# print(lemmatizer.lemmatize("loving")) #loving
+# print(lemmatizer.lemmatize("swimming", "v")) #love
+# print(lemmatize_sentence("i will be working from home")) #I be love it
 #
 #
-# def sentence_similarity(sentence1, sentence2):
-#     """ compute the sentence similarity using Wordnet """
-#     # Tokenize and tag
-#     sentence1 = pos_tag(word_tokenize(sentence1))
-#     sentence2 = pos_tag(word_tokenize(sentence2))
-#
-#     # Get the synsets for the tagged words
-#     synsets1 = [tagged_to_synset(*tagged_word) for tagged_word in sentence1]
-#     synsets2 = [tagged_to_synset(*tagged_word) for tagged_word in sentence2]
-#
-#     # Filter out the Nones
-#     synsets1 = [ss for ss in synsets1 if ss]
-#     synsets2 = [ss for ss in synsets2 if ss]
-#     print(synsets1)
-#     print(synsets2)
-#
-#     score, count = 0.0, 0
-#
-#     # For each word in the first sentence
-#     for synset in synsets1:
-#         # Get the similarity value of the most similar word in the other sentence
-#         best_score = print([synset.path_similarity(ss) for ss in synsets2])
-#
-#         # Check that the similarity could have been computed
-#         if best_score is not None:
-#             score += best_score
-#             count += 1
-#
-#     # Average the values
-#     score /= count
-#     return score
-#
-#
-# sentences = [
-#     "Dogs are awesome.",
-#     "Some gorgeous creatures are felines.",
-#     "Dolphins are swimming mammals.",
-#     "Cats are beautiful animals.",
-# ]
-#
-# focus_sentence = "Cats are beautiful animals."
-#
-# for sentence in sentences:
-#     print(focus_sentence, sentence, sentence_similarity(focus_sentence, sentence))
-#     print(sentence, focus_sentence, sentence_similarity(sentence, focus_sentence))
-#
-#
-#
-#
-#
-#
+# # # import these modules
+# # from nltk.stem import WordNetLemmatizer
 # #
+# # lemmatizer = WordNetLemmatizer()
 # #
+# # print("swimming :", lemmatizer.lemmatize("i want to go swimming"))
+# # print("corpora :", lemmatizer.lemmatize("corpora"))
 # #
+# # # a denotes adjective in "pos"
+# # print("swim :", lemmatizer.lemmatize("swimming", pos="v"))
+#
+#
+# # import nltk
+# # from nltk.corpus import stopwords,wordnet
+# # from nltk.tokenize import word_tokenize, sent_tokenize
+# # from nltk.tag import DefaultTagger
+# # import spacy
 # #
+# # stop_words = set(stopwords.words('english'))
+# # def removeStopWords(sentence):
+# #     filtered_sentence = " ".join(filter(lambda word: word not in stop_words, sentence.split()))
+# #     return filtered_sentence
 # #
+# # nlp = spacy.load('en_core_web_lg')
+# # a = "i ducked"
+# # a1=nlp(removeStopWords(a))
+# # print(a1)
+# # b = "the duck is yellow"
+# # b1=nlp(removeStopWords(b))
+# # print(b1)
+# # print(nlp(a).similarity(nlp(b)))
+# # print(a1.similarity(b1))
 # #
-# # # from nltk.corpus import wordnet as wn
+#
+#
+# # # # Program to measure similarity between
+# # # # two sentences using cosine similarity.
+# # # from nltk.corpus import stopwords
+# # # from nltk.tokenize import word_tokenize
 # # #
-# # # print(wn.synsets('cat', 'n'))
-# # # print(wn.synsets('dog', 'n'))
-# # # print(wn.synsets('feline', 'n'))
-# # # print(wn.synsets('mammal', 'n'))
+# # # # X = input("Enter first string: ").lower()
+# # # # Y = input("Enter second string: ").lower()
+# # # X = "I want to swim"
+# # # Y = "I wishing to swim"
 # # #
-# # # cat = wn.synsets('cat', 'n')[0]  # Get the most common synset
-# # # print(cat.lemmas()[0].count())  # Get the first lemma => 18
+# # # # tokenization
+# # # X_list = word_tokenize(X)
+# # # Y_list = word_tokenize(Y)
 # # #
-# # # dog = wn.synsets('dog', 'n')[0]  # Get the most common synset
-# # # feline = wn.synsets('feline', 'n')[0]  # Get the most common synset
-# # # mammal = wn.synsets('mammal', 'n')[0]  # Get the most common synset
+# # # # sw contains the list of stopwords
+# # # sw = stopwords.words('english')
+# # # l1 = [];
+# # # l2 = []
 # # #
-# # # for synset in [dog, feline, mammal]:
-# # #     print("Similarity(%s, %s) = %s" % (cat, synset, cat.wup_similarity(synset)))
+# # # # remove stop words from string
+# # # X_set = {w for w in X_list if not w in sw}
+# # # Y_set = {w for w in Y_list if not w in sw}
+# # #
+# # # # form a set containing keywords of both strings
+# # # rvector = X_set.union(Y_set)
+# # # for w in rvector:
+# # #     if w in X_set:
+# # #         l1.append(1)  # create a vector
+# # #     else:
+# # #         l1.append(0)
+# # #     if w in Y_set:
+# # #         l2.append(1)
+# # #     else:
+# # #         l2.append(0)
+# # # c = 0
+# # #
+# # # # cosine formula
+# # # for i in range(len(rvector)):
+# # #     c += l1[i] * l2[i]
+# # # cosine = c / float((sum(l1) * sum(l2)) ** 0.5)
+# # # print("similarity: ", cosine)
+# #
+# # from nltk import word_tokenize, pos_tag
+# # from nltk.corpus import wordnet as wn
+# #
+# #
+# # def penn_to_wn(tag):
+# #     """ Convert between a Penn Treebank tag to a simplified Wordnet tag """
+# #     if tag.startswith('N'):
+# #         return 'n'
+# #
+# #     if tag.startswith('V'):
+# #         return 'v'
+# #
+# #     if tag.startswith('J'):
+# #         return 'a'
+# #
+# #     if tag.startswith('R'):
+# #         return 'r'
+# #
+# #     return None
+# #
+# #
+# # def tagged_to_synset(word, tag):
+# #     wn_tag = penn_to_wn(tag)
+# #     if wn_tag is None:
+# #         return None
+# #
+# #     try:
+# #         return wn.synsets(word, wn_tag)[0]
+# #     except:
+# #         return None
+# #
+# #
+# # def sentence_similarity(sentence1, sentence2):
+# #     """ compute the sentence similarity using Wordnet """
+# #     # Tokenize and tag
+# #     sentence1 = pos_tag(word_tokenize(sentence1))
+# #     sentence2 = pos_tag(word_tokenize(sentence2))
+# #
+# #     # Get the synsets for the tagged words
+# #     synsets1 = [tagged_to_synset(*tagged_word) for tagged_word in sentence1]
+# #     synsets2 = [tagged_to_synset(*tagged_word) for tagged_word in sentence2]
+# #
+# #     # Filter out the Nones
+# #     synsets1 = [ss for ss in synsets1 if ss]
+# #     synsets2 = [ss for ss in synsets2 if ss]
+# #     print(synsets1)
+# #     print(synsets2)
+# #
+# #     score, count = 0.0, 0
+# #
+# #     # For each word in the first sentence
+# #     for synset in synsets1:
+# #         # Get the similarity value of the most similar word in the other sentence
+# #         best_score = print([synset.path_similarity(ss) for ss in synsets2])
+# #
+# #         # Check that the similarity could have been computed
+# #         if best_score is not None:
+# #             score += best_score
+# #             count += 1
+# #
+# #     # Average the values
+# #     score /= count
+# #     return score
+# #
+# #
+# # sentences = [
+# #     "Dogs are awesome.",
+# #     "Some gorgeous creatures are felines.",
+# #     "Dolphins are swimming mammals.",
+# #     "Cats are beautiful animals.",
+# # ]
+# #
+# # focus_sentence = "Cats are beautiful animals."
+# #
+# # for sentence in sentences:
+# #     print(focus_sentence, sentence, sentence_similarity(focus_sentence, sentence))
+# #     print(sentence, focus_sentence, sentence_similarity(sentence, focus_sentence))
+# #
+# #
+# #
+# #
+# #
+# #
+# # #
+# # #
+# # #
+# # #
+# # #
+# # #
+# # # # from nltk.corpus import wordnet as wn
+# # # #
+# # # # print(wn.synsets('cat', 'n'))
+# # # # print(wn.synsets('dog', 'n'))
+# # # # print(wn.synsets('feline', 'n'))
+# # # # print(wn.synsets('mammal', 'n'))
+# # # #
+# # # # cat = wn.synsets('cat', 'n')[0]  # Get the most common synset
+# # # # print(cat.lemmas()[0].count())  # Get the first lemma => 18
+# # # #
+# # # # dog = wn.synsets('dog', 'n')[0]  # Get the most common synset
+# # # # feline = wn.synsets('feline', 'n')[0]  # Get the most common synset
+# # # # mammal = wn.synsets('mammal', 'n')[0]  # Get the most common synset
+# # # #
+# # # # for synset in [dog, feline, mammal]:
+# # # #     print("Similarity(%s, %s) = %s" % (cat, synset, cat.wup_similarity(synset)))
